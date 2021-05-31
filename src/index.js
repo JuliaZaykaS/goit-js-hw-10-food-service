202f2bb689df27edca1e0ed090f7c77cdb07a9da
JuliaZaykaS <sys55@mail.ru>
2021-05-31 14:47:46 +0300

    сделала разметку

--- src/index.js
+++ src/index.js
@@ -2,3 +2,8 @@ import cardTpl from './templates/card.hbs';
 import menu from './menu.json';
 
 console.log(cardTpl(menu));
+const menuMarkup = cardTpl(menu);
+
+const menuListEl = document.querySelector('.js-menu');
+menuListEl.insertAdjacentHTML('beforeend', menuMarkup);
+
