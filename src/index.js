a70ded421bea6ae25b4fa07d5503f8616305adef
JuliaZaykaS <sys55@mail.ru>
2021-06-01 10:20:06 +0300

    доделала

--- src/index.js
+++ src/index.js
@@ -3,75 +3,65 @@ import menu from './menu.json';
 
 // Шаблон разметки
 
-// console.log(cardTpl(menu));
 const menuMarkup = cardTpl(menu);
 
 const menuListEl = document.querySelector('.js-menu');
 menuListEl.insertAdjacentHTML('beforeend', menuMarkup);
 
+const Theme = { LIGHT: 'light-theme', DARK: 'dark-theme', };
 
 // Смена темы
 const checkboxEl = document.querySelector('#theme-switch-toggle');
-console.log(checkboxEl);
-
-// console.log(checkboxEl.getAttribute('checked'));
 
 checkboxEl.addEventListener('change', OnCheckboxChange);
 
-const Theme = { LIGHT: 'light-theme', DARK: 'dark-theme', };
 
-// document.body.classList.add(Theme.LIGHT);
-populateTextarea();
+savedUserTheme();
 
-function OnCheckboxChange(evt) {
-    // const checkboxValue = evt.target.value;
-    // console.log(checkboxValue);
-    // const checkboxValue = evt.target.value;
-    // document.body.classList.add(Theme.LIGHT);
-
+function OnCheckboxChange() {
 
     if (!document.body.classList.contains(Theme.LIGHT)) {
-        document.body.classList.add(Theme.LIGHT);
-        localStorage.setItem('LIGHT', Theme.LIGHT);
-        localStorage.removeItem('DARK');
-        checkboxEl.removeAttribute('checked');
+
+        addLightTheme();
+
+        localStorage.setItem('theme', Theme.LIGHT);
         localStorage.removeItem('checked');
     }
 
-    document.body.classList.toggle(Theme.DARK);
+    addDarkTheme();
+
     if (document.body.classList.contains(Theme.DARK)) {
         document.body.classList.remove(Theme.LIGHT);
 
-        checkboxEl.setAttribute('checked', true);
-        localStorage.setItem('checked', true);
-        localStorage.setItem('DARK', Theme.DARK);
-        localStorage.removeItem('LIGHT');
+        localStorage.setItem('theme', Theme.DARK);
+        localStorage.setItem('checked', checkboxEl.checked);
+
     }
 
 }
-function populateTextarea() {
+
+// Получение темы из localStorage
+function savedUserTheme() {
 
     const checkedValue = localStorage.getItem('checked');
-    console.log(checkedValue);
+
     if (checkedValue) {
-
-         localStorage.setItem('DARK', Theme.DARK);
-
+        localStorage.setItem('theme', Theme.DARK);
+        checkboxEl.checked = true;
+        addDarkTheme();
     }
 }
-// Сохранение темы в localStorage
 
+// Функции добавления тем
+function addDarkTheme() {
+    document.body.classList.toggle(Theme.DARK);
 
+}
 
-// if (document.body.classList.contains('light-theme')) {
+function addLightTheme() {
+    document.body.classList.toggle(Theme.LIGHT);
 
-//     localStorage.setItem(Theme.LIGHT);
-//     localStorage.removeItem(Theme.DARK);
-// } else {
-
-//     localStorage.setItem(Theme.DARK, [Theme.DARK]);
-//     localStorage.removeItem(Theme.LIGHT);
-// }
+}
 
 
 
